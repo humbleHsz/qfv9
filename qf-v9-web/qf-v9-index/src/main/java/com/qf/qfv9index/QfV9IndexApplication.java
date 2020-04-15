cbe749decae98de21ec6f39c8fb0d13bef99f09a
黄诗泽 <hsz@spacetech.com.cn>
2020-04-15 18:42:04 +0800

    uodate

--- qf-v9-web/qf-v9-index/src/main/java/com/qf/qfv9index/QfV9IndexApplication.java
+++ qf-v9-web/qf-v9-index/src/main/java/com/qf/qfv9index/QfV9IndexApplication.java
@@ -1,9 +1,11 @@
 package com.qf.qfv9index;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubbo
 public class QfV9IndexApplication {
 
     public static void main(String[] args) {
